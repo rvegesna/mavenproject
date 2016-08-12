f59c1ff4682dd1b7123076bd798ec381885cb4f8
ravi <ravi@localhost>
2016-08-12 19:58:37 +0530

    v2

---
+++ src/main/java/net/systemshub/User.java
@@ -0,0 +1,32 @@
+package net.systemshub;
+
+public class User {
+public String name,password,email;
+
+public User(){}
+public void setname (String name){
+        this.name= name;
+        }
+public void setpassword(String password){
+        this.password=password;
+
+}
+public void setemail(String email){
+        this.email=email;
+
+}
+
+public String getname(){
+        return name;
+
+}
+public String getpassword(){
+        return password;
+}
+public String getemail(){
+        return email;
+
+}
+
+
+}
